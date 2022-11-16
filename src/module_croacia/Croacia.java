071e9e1aa27f9fb3326c0664a2511a80b5f7a938
Nicolas Zimermann <nzimermann@proton.me>
2022-11-16 09:16:27 -0300

    Initial Commit

---
+++ src/module_croacia/Croacia.java
@@ -0,0 +1,61 @@
+package module_croacia;
+
+import java.awt.Image;
+import java.nio.file.Path;
+
+import fifa.NationalTeamInfos;
+import fifa.NationalTeamStats;
+
+public class Croacia implements NationalTeamInfos {
+
+	public int getHowManyMembers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getOldestPlayer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getYoungestPlayer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getAverageAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getPlayer(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPressOfficerContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getCountryName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Image getFlagImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Path getTechnicalCommittee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public NationalTeamStats getStatsResponsible() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
