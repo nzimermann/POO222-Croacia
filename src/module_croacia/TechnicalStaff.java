645a765e294130fbdc0fa27bb1001d27a1da76f5
Nicolas Zimermann <nzimermann@proton.me>
2022-11-20 18:59:23 -0300

    Add class TechnicalStaff.java

---
+++ src/module_croacia/TechnicalStaff.java
@@ -0,0 +1,70 @@
+package module_croacia;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class TechnicalStaff {
+	private String name;
+	private String nickname;
+	private String role;
+	private LocalDate birthDate;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		if (name == null || name.isBlank()) {
+			throw new IllegalArgumentException("Empty name");
+		}
+		this.name = name;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setNickname(String nickname) {
+		if (nickname == null || nickname.isBlank()) {
+			throw new IllegalArgumentException("Empty nickname");
+		}
+		this.nickname = nickname;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	
+	public void setRole(String role) {
+		if (role == null || role.isBlank()) {
+			throw new IllegalArgumentException("Empty role");
+		}
+		this.role = role;
+	}
+	
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDate(LocalDate birthDate) {
+		if (birthDate == null) {
+			throw new IllegalArgumentException("Empty birth date");
+		}
+		this.birthDate = birthDate;
+	}
+	
+	public int getAge() {
+		if (birthDate == null) {
+			throw new IllegalArgumentException("Empty birth date");
+		}
+		return Period.between(birthDate, LocalDate.now()).getYears();
+	}
+	
+	public String getDataJSON() {
+		return "{name:\""
+				+name+"\", nickname:\""
+				+nickname+"\", role:\""
+				+role+"\", age:"
+				+this.getAge()+"}";
+	}
+}
