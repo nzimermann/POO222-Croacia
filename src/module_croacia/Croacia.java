705997f510039f256d1b4e20dda21af5da3c86e5
Nicolas Zimermann <nzimermann@proton.me>
2022-11-20 21:14:41 -0300

    Croacia.java partial

--- src/module_croacia/Croacia.java
+++ src/module_croacia/Croacia.java
@@ -2,35 +2,56 @@ package module_croacia;
 
 import java.awt.Image;
 import java.nio.file.Path;
-
 import fifa.NationalTeamInfos;
 import fifa.NationalTeamStats;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Croacia implements NationalTeamInfos {
+	private HashMap<Integer, Player> players = new HashMap<Integer, Player>();
+	private ArrayList<TechnicalStaff> technicallStaffs = new ArrayList<TechnicalStaff>();
+	private Manager manager;
 
 	public int getHowManyMembers() {
-		// TODO Auto-generated method stub
-		return 0;
+		int mgr = 0;
+		if (manager != null) {mgr = 1;}
+		return players.size() + technicallStaffs.size() + mgr;
 	}
 
 	public int getOldestPlayer() {
-		// TODO Auto-generated method stub
-		return 0;
+		int max = Integer.MIN_VALUE;
+		int playerNumber = 0;
+		for (Player p: players.values()) {
+			if (p.getAge() > max) {
+				max = p.getAge();
+				playerNumber = p.getNumber();
+			}
+		}
+		return playerNumber;
 	}
 
 	public int getYoungestPlayer() {
-		// TODO Auto-generated method stub
-		return 0;
+		int min = Integer.MAX_VALUE;
+		int playerNumber = 0;
+		for (Player p: players.values()) {
+			if (p.getAge() < min) {
+				min = p.getAge();
+				playerNumber = p.getNumber();
+			}
+		}
+		return playerNumber;
 	}
 
 	public double getAverageAge() {
-		// TODO Auto-generated method stub
-		return 0;
+		double sumAge = 0;
+		for (Player p: players.values()) {
+			sumAge += p.getAge();
+		}
+		return sumAge/players.size();
 	}
 
 	public String getPlayer(int number) {
-		// TODO Auto-generated method stub
-		return null;
+		return players.get(number).getDataJSON();
 	}
 
 	public String getPressOfficerContacts() {
@@ -39,8 +60,7 @@ public class Croacia implements NationalTeamInfos {
 	}
 
 	public String getCountryName() {
-		// TODO Auto-generated method stub
-		return null;
+		return manager.getDataJSON();
 	}
 
 	public Image getFlagImage() {
