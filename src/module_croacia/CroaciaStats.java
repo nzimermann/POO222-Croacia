bcf1f4c8aa009099d6b6e133c86218f7221a017e
Nicolas Zimermann <nzimermann@proton.me>
2022-11-29 20:50:03 -0300

    Marco3

---
+++ src/module_croacia/CroaciaStats.java
@@ -0,0 +1,15 @@
+package module_croacia;
+
+import fifa.NationalTeamStats;
+
+public class CroaciaStats implements NationalTeamStats {
+	
+	public int getHowManyQuestions() {
+		return 1;
+	}
+
+	public int getHowManyCallsToPlayer(int number) {
+		return 1;
+	}
+	
+}
