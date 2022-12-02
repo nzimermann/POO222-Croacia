8499696af4251fc4e379f5a8d463a38c77026875
Nicolas Zimermann <nzimermann@proton.me>
2022-12-02 20:53:56 -0300

    Update CroaciaStats.java

--- src/module_croacia/CroaciaStats.java
+++ src/module_croacia/CroaciaStats.java
@@ -1,15 +1,32 @@
 package module_croacia;
 
 import fifa.NationalTeamStats;
+import java.util.HashMap;
 
 public class CroaciaStats implements NationalTeamStats {
-	
+	private HashMap<Integer, Integer> playerRequestCount = new HashMap<>();
+	private int requestCount = 0;
+
+	protected void addPlayer(int number) {
+		playerRequestCount.putIfAbsent(number, 0);
+	}
+
+	protected void incRequestCount() {
+		requestCount++;
+	}
+
+	protected void incPlayerRequestCount(int number) {
+		if (playerRequestCount.containsKey(number)) {
+			playerRequestCount.put(number, playerRequestCount.get(number) + 1);
+		}
+	}
+
 	public int getHowManyQuestions() {
-		return 1;
+		return requestCount;
 	}
 
 	public int getHowManyCallsToPlayer(int number) {
-		return 1;
+		return playerRequestCount.get(number);
 	}
-	
+
 }
